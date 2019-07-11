fc302f83cc89c36fb6ab8d77ab948f836df2ba13
pighaddt <pighaddt@gmail.com>
2019-07-11 10:57:40 +0800

    calculate us & ntd

--- app/src/main/java/com/home/currency/MainActivity.java
+++ app/src/main/java/com/home/currency/MainActivity.java
@@ -1,13 +1,53 @@
 package com.home.currency;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private EditText edNtd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findView();
     }
+
+    public void go(View view){
+        String s = edNtd.getText().toString();
+        if (s.equals("")){
+            new AlertDialog.Builder(this)
+                    .setTitle("Problem")
+                    .setMessage("Please enter you NTD amout")
+                    .setPositiveButton("OK", null)
+                    .show();
+        }
+        else{
+            float ntd = Float.parseFloat(s);
+            float us = ntd * 30.9f;
+            new AlertDialog.Builder(this)
+                    .setTitle("Result")
+                    .setMessage("USD is " + us)
+                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            edNtd.setText("");
+                        }
+                    })
+                    .show();
+        }
+
+    }
+
+    private void findView() {
+        edNtd = findViewById(R.id.ntd);
+    }
+
 }
